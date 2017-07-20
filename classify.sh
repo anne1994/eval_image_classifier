a5f71a80b42d27dd67bc3409e7282a6a24ac4a8b
anne1994 <anneangelina94@gmail.com>
2017-07-19 17:45:31 -0700

    Update classify.sh

--- classify.sh
+++ classify.sh
@@ -2,5 +2,5 @@ python classification3.py \
 --num_classes 4 \
 --infile in.txt \
 --model_name inception_v3 \
---checkpoint_path /data/tmp/prescan-models/inception_v3/all/model.ckpt-500 \
+--checkpoint_path /data/tmp/models/inception_v3/all/model.ckpt-500 \
 --outfile new.txt
