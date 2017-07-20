ee5692cba22361741efa5db43b42cf4a98af3cd0
anne1994 <anneangelina94@gmail.com>
2017-07-19 17:45:01 -0700

    Add files via upload

---
+++ classify.sh
@@ -0,0 +1,6 @@
+python classification3.py \
+--num_classes 4 \
+--infile in.txt \
+--model_name inception_v3 \
+--checkpoint_path /data/tmp/prescan-models/inception_v3/all/model.ckpt-500 \
+--outfile new.txt
